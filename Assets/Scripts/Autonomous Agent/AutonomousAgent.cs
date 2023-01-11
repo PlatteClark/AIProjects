2e6f3c34053b72829f449e4c74c396fa79cfdaa7
Platte Clark <plattinum95@gmail.com>
2023-01-10 18:29:25 -0700

    AI Agent

---
+++ Assets/Scripts/Autonomous Agent/AutonomousAgent.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class AutonomousAgent : Agent
+{
+    void Update()
+    {
+        var gameObjects = perception.GetGameObjects();
+        foreach (var gameObject in gameObjects)
+        {
+            Debug.DrawLine(transform.position, gameObject.transform.position);
+        }
+        if (gameObjects.Length > 0) 
+    {
+            Vector3 direction = (gameObjects[0].transform.position - transform.position).normalized;
+            movement.ApplyForce(direction * 2);
+        }
+
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
+    }
+}
